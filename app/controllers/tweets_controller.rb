a32594e102ce0e4f58c854916af52a87e8f94bde
taka president <life.i.s.w---@i.softbank.jp>
2019-08-21 15:01:29 +0900

    一対一及び一対多設定、controller,viewの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,12 +9,24 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
+    #フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetを代入
+    @tweet = Tweet.new(tweet_params)
+    #user_idの情報はフォームからは来ていないので、deviseメソッドを使って、「ログインしている自分のid」を代入
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def show
     @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
   end
 
+  private
+    def tweet_params
+
+      params.require(:tweet).permit(:body) # tweetモデルのカラムのみを許可
+    end
 end
