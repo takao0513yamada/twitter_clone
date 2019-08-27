14d0f412a971d6911e93ee389ed6b48ddc640d64
taka president <life.i.s.w---@i.softbank.jp>
2019-08-28 06:27:11 +0900

    setting for routes(comments)

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@ Rails.application.routes.draw do
 
   get 'comments/create'
   get 'comments/destroy'
-  root 'tweets#index'
+  get 'favorites/create'
+  get 'favorites/destroy'
+  root  to: 'tweets#index'
   devise_for :users
 
   resources :tweets do
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   resources :tweets
   resources :users
+
+  resources :tweets do
+    resource :comments, only: [:create, :destroy]
+  end
 end
