28e73201f3f235cb48a0e2c122558a4193ef9f7d
taka president <life.i.s.w---@i.softbank.jp>
2019-09-07 11:15:09 +0900

    コメント内容の表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(user_id: current_user.id, tweet_id: params[:tweet_id], body: params[:body])
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    comment.tweet_id = params[:tweet_id]
+    comment.save
+
     redirect_to tweet_path(params[:tweet_id])
   end
 
   def destroy
     Comment.destroy(id: params[:id])
     redirect_to tweet_path(params[:tweet_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
